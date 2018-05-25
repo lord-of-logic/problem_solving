279e27ab87e976f058dd47cf401d82e75d45aedb
Ranjith P <pranjith0869@gmail.com>
2018-05-25 15:55:36 +0530

    Create postorder.cpp

---
+++ trees/postorder.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class node
+{
+public:
+    char data;
+    node *left;
+    node*right;
+    node(char a)
+    {
+        this->data=a;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+};
+
+void postorder(struct node*root)
+{
+    if(!root)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main()
+{
+    node *root=new node('a');
+    root->left=new node('b');
+    root->right=new node('c');
+    root->left->left=new node('d');
+    root->left->right=new node('e');
+    root->right->left=new node('f');
+    root->right->right=new node('g');
+    cout<<"Post-order traversal of given binary tree is: ";
+    postorder(root);
+    return 0;
+}
